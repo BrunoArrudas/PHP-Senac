5408141792469698cbcdd00d57c4fee4e78bcbce
BrunoArrudas <you@example.com>
2024-04-04 20:41:48 -0300

    13

--- 13/index.php
+++ 13/index.php
@@ -32,6 +32,25 @@ class Cliente extends Conta{
     private $cpf;
 }
 
+// Criando instâncias de Cliente
+$cliente1 = new Cliente("João", "123456789-00");
+$cliente2 = new Cliente("Maria", "987654321-00");
+
+// Cirando instâncias de Contas
+$contaCorrente1 = new ContaCorrente($cliente1, 1001, 1500, 500);
+$contaPouapanca1 = new ContaPoupanca($cliente1, 2001, 3000, 0.05);
+$contaCorrente2 = new ContaCorrente($cliente2, 1002, 3000, 1000);
+
+// Realizando operações nas contas
+$contaCorrente1->sacar(100);
+$contaCorrente2->transferir(200, $contaPouapanca1);
+$contaPouapanca1->aplicarRendimento();
+
+// Exibindo dados das contas:
+echo "Dados da Conta Corrente 1: $contaCorrente1 <br>";
+echo "Dados da Conta Poupança 1: $contaPouapanca1 <br>";
+echo "Dados da Conta Corrente 2: $contaCorrente2 <br>";
+
 
 
 
