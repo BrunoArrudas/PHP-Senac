fbdfa1483d841c6370d44557dc742fdf799e8ecc
BrunoArrudas <you@example.com>
2024-04-04 20:36:07 -0300

    Conta bancaria 13

--- 13/index.php
+++ 13/index.php
@@ -1,5 +1,35 @@
 <?php
 
+abstract class Conta {
+
+    private $numeroConta;
+    private $saldo;
+    private $cliente;
+
+}
+
+class contaCorrente extends Conta{
+    private $limiteCheque;
+
+    private function sacar(){
+
+    }
+}
+
+
+class contaPoupanca extends Conta{
+
+    private $taxaRendimento;
+    
+    private function rendimento(){
+
+    }
+
+}
+
+class Cliente extends Conta{
+    
+}
 
 
 
