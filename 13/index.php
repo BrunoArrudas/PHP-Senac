af3e99585ce38649d8e7efd739abb4bbccce5d44
BrunoArrudas <you@example.com>
2024-04-05 22:08:40 -0300

    13

--- 13/index.php
+++ 13/index.php
@@ -1,7 +1,6 @@
 <?php
 // Definição da classe Cliente
-class Cliente
-{
+class Cliente {
     // Atributos
     private $nome;
     private $cpf;
@@ -12,27 +11,32 @@ class Cliente
         $this->nome = $nome;
         $this->cpf = $cpf;
     }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function __toString() {
+        return "Nome: $this->nome - CPF: $this->cpf";
+    }
 }
 
-abstract class Conta
-{
+abstract class Conta {
     // Atributos
     protected $cliente;
     protected $numero;
     protected $saldo;
 
     // Método Construtor
-    public function __construct($cliente, $numero, $saldo)
-    {
+    public function __construct($cliente, $numero, $saldo) {
         $this->cliente = $cliente;
         $this->numero = $numero;
         $this->saldo = $saldo;
     }
 
     // Método para sacar
-    public function sacar($valorSaque)
-    {
-        if ($this->saldo >= $valorSaque && $valorSaque > 0) {
+    public function sacar($valorSaque) {
+        if($this->saldo >= $valorSaque && $valorSaque > 0) {
             $this->saldo -= $valorSaque;
             return true;
         }
@@ -41,9 +45,8 @@ abstract class Conta
     }
 
     // Método para depositar
-    public function depositar($valorDeposito)
-    {
-        if ($valorDeposito > 0) {
+    public function depositar($valorDeposito) {
+        if($valorDeposito > 0) {
             $this->saldo += $valorDeposito;
             return true;
         }
@@ -52,58 +55,59 @@ abstract class Conta
     }
 
     // Método para transferir
-    public function transferir($valorTransferencia, $contaDestino)
-    {
-        if ($this->sacar($valorTransferencia)) {
+    public function transferir($valorTransferencia, $contaDestino) {
+        if($this->sacar($valorTransferencia)) {
             $contaDestino->depositar($valorTransferencia);
             return true;
         }
 
         return false;
     }
+
+    // Método para exibir dados da conta
+    public function __toString() {
+        return "Numero: $this->numero - Saldo: $this->saldo - Cliente: {$this->cliente->getNome() }";
+    }
 }
 
 // Definição da classe ContaCorrente, que herda de Conta
-class ContaCorrente extends Conta
-{
+class ContaCorrente extends Conta {
     // Atributos específicos da ContaCorrente
     private $limiteChequeEspecial;
 
     // Método construtor
-    public function __construct($cliente, $numero, $saldo, $limiteChequeEspecial)
-    {
+    public function __construct($cliente, $numero, $saldo, $limiteChequeEspecial) {
         parent::__construct($cliente, $numero, $saldo);
         $this->limiteChequeEspecial = $limiteChequeEspecial;
     }
 
-    //Mtodo para sacar com o limite do cheque especial (Virtual)
-
-    public function sacar($valorSaque)
-    {
+    // Método para com limite do cheque especial (Virtual)
+    public function sacar($valorSaque) {
         $valorLimiteEspecial = $this->saldo + $this->limiteChequeEspecial;
-        if ($valorSaque <= $valorLimiteEspecial) {
-            return parent::sacar($valorLimiteEspecial);
+        if($valorSaque <= $valorLimiteEspecial) {
+            return parent::sacar($valorSaque);
         }
+
         return false;
     }
 }
 
 // Definição da classe ContaPuapanca, que herda de Conta
-class ContaPoupanca extends Conta
-{
+class ContaPoupanca extends Conta {
+    // Atributos específicos da ContaPoupanca
+    private $taxaRendimento;
+
+    // Método construtor
+    public function __construct($cliente, $numero, $saldo, $taxaRendimento) {
+        parent::__construct($cliente, $numero, $saldo);
+        $this->taxaRendimento = $taxaRendimento;
+    }
+
+    // Metodo para aplicar rendimetno
+    public function aplicarRendimento() {
+        $this->saldo += $this->saldo * $this->taxaRendimento;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Criando instâncias de Cliente
 $cliente1 = new Cliente("João", "123456789-00");
@@ -116,6 +120,7 @@ $contaCorrente2 = new ContaCorrente($cliente2, 1002, 3000, 1000);
 
 // Realizando operações nas contas
 $contaCorrente1->sacar(100);
+$contaPouapanca1->sacar(200);
 $contaCorrente2->transferir(200, $contaPouapanca1);
 $contaPouapanca1->aplicarRendimento();
 
@@ -123,3 +128,14 @@ $contaPouapanca1->aplicarRendimento();
 echo "Dados da Conta Corrente 1: $contaCorrente1 <br>";
 echo "Dados da Conta Poupança 1: $contaPouapanca1 <br>";
 echo "Dados da Conta Corrente 2: $contaCorrente2 <br>";
+
+echo "Deposito de 1000 reais em cada conta: <br>";
+$contaCorrente1->depositar(1000);
+$contaPouapanca1->depositar(1000);
+$contaCorrente2->depositar(1000);
+
+// Exibindo dados das contas pós depósitos:
+echo "Dados da Conta Corrente 1: $contaCorrente1 <br>";
+echo "Dados da Conta Poupança 1: $contaPouapanca1 <br>";
+echo "Dados da Conta Corrente 2: $contaCorrente2 <br>";
+?>
