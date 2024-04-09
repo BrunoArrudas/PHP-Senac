158d94a86dd2b90875432668c746cad680d6579d
BrunoArrudas <you@example.com>
2024-04-08 21:25:34 -0300

    Aula 14.01 - Conexão basica

---
+++ 14/01_conexao.php
@@ -0,0 +1,22 @@
+<?php
+//Conexão Direta
+$connection = new mysqli("localhost", "usuario_contatos","senha123","contatos");
+
+//Verificando a conexão
+if(!$connection->connect_errno){
+    echo "Sucesso na conexão! <br>";
+}
+
+if($connection->connect_errno){
+    echo "Erro de conexão! <br>";
+    echo "Erro ". mysqli_connect_error();
+}
+
+
+
+
+
+
+
+
+?>
