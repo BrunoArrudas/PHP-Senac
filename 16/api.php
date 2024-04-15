1abeb58c4d5f0f628b345934674311cd305febd7
BrunoArrudas <you@example.com>
2024-04-15 20:58:23 -0300

    Atualização "Repository contacts static completos"

--- 16/api.php
+++ 16/api.php
@@ -1,5 +1,8 @@
 <?php
 require_once 'DatabaseRepository.php';
 
-print_r(DatabaseRepository::getAllContacts());
+//print_r(DatabaseRepository::getContactById(4));
+//echo DatabaseRepository::insertContact("Bruno Chaveiro", "11 99756688", "bruno_chave_gold@mail.com");
+//echo DatabaseRepository::updateContact(9, "Pessa B", "11 99755555", "pessoa_b@mail.com");
+echo DatabaseRepository::deleteContact(11);
 ?>
