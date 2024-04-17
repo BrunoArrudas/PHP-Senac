9dd3bd78777b07a83cf1477cff7396d6ce601f49
BrunoArrudas <you@example.com>
2024-04-16 21:26:36 -0300

    16.0

--- 15/api.php
+++ 15/api.php
@@ -1,41 +1,37 @@
 <?php
 require_once 'DatabaseRepository.php';
 
-$repository = new DatabaseRepository();
-$action = isset($_GET ['action']) ? $_GET ['action'] : '';
+$action = $_GET['action'];
 
-
-switch($action){
+switch($action) {
     case 'list':
-        echo json_encode($repository->getAllItems());
+        echo json_encode(DatabaseRepository::getAllContacts());
         break;
-
+    case 'get':
+        $id = $_GET['id'];
+        echo json_encode(DatabaseRepository::getContactById($id));
+        break;
+    case 'getNome':
+        $nome = $_GET['nome'];
+        echo json_encode(DatabaseRepository::getContactByName($nome));
+        break;
     case 'add':
-        $data = json_decode(file_get_contents('php://input',true));
-        $nomeProduto = $data->nome_produto;
-        $quantidade = $data->quantidade;
-        $repository->addItem($nomeProduto, $quantidade);
-        echo json_decode($repository->addItem($nomeProduto, $quantidade));
-        break;
-       
-    case 'compraItem':
         $data = json_decode(file_get_contents('php://input', true));
-        $id = $data->id;
-        $comprado = $data->comprado;
-        echo json_decode($repository->compraItem($id, $comprado));
+        $success = DatabaseRepository::insertContact($data->nome, $data->telefone, $data->email);
+        echo json_encode(['scuccess' => $success]);
         break;
-
-    case 'delete';
-        $id = $_GET ['id'];
-        echo json_decode($repository->deleteItem($id));
+    case 'update':
+        $data = json_decode(file_get_contents('php://input', true));
+        $success = DatabaseRepository::updateContact($data->id, $data->nome, $data->telefone, $data->email);
+        echo json_encode(['success' => $success]);
         break;
-
-
-
-
-
-
-    } 
-
+    case 'delete':
+        $id = $_GET['id'];
+        $success = DatabaseRepository::deleteContact($id);
+        echo json_encode(['success' => $success]);
+        break;
+    default:
+        echo json_encode(['error' => 'Ação inválida']);
+}
 
 ?>
