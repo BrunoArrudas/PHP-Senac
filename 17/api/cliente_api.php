c60dc00c09dee96edf79f5875fb00dfc5daf2b5c
BrunoArrudas <you@example.com>
2024-04-18 21:23:23 -0300

    17

--- 17/api/cliente_api.php
+++ 17/api/cliente_api.php
@@ -4,18 +4,27 @@ require_once '../database/ClienteRepository.php';
 
 $action = $_GET['action'];
 
-switch($action){
-    
+switch($action) {
     case 'listar':
-        echo json_encode(ClienteRepository::getAllClientes());
+        listarClientes();
         break;
-
+    case 'buscar':
+        buscarClientePorId();
+        break;
+    case 'cadastrar':
+        cadastrarCliente();
+        break;
+    case 'atualizar':
+        atualizarCliente();
+    case 'excluir':
+        excluirCliente();
+        break;
     default:
-    http_response_code(400); //Requisição invalida
-    echo json_encode(['error' => 'ação invalida']);
+        http_response_code(400); // Requisição inválida
+        echo json_encode(['error' => 'Ação inválida']);
 }
 
-function listarClientes(){
+function listarClientes() {
     $clientes = ClienteRepository::getAllClientes();
     echo json_encode($clientes);
 }
@@ -33,14 +42,10 @@ function atualizarCliente() {
 }
 
 function excluirCliente() {
-    
-}
 
+}
 // Create Read Update Ddelete
 // Create = POST (INSERT)
 // Read = GET (SELECT)
 // Update = POST(GET) (UPDATE SET)
 // Delete = POST (DELETE)
-
-
-?>
