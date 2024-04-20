6cf28f16faa43afde5d623a8b018a4c9168b6faf
BrunoArrudas <you@example.com>
2024-04-19 21:15:01 -0300

    17

--- 17/api/conta_api.php
+++ 17/api/conta_api.php
@@ -4,17 +4,86 @@ require_once '../database/ContaRepository.php';
 $action = $_GET['action'];
 
 switch($action) {
+    
     case 'listar':
-        listarContas();
+        listarClientes();
+        break;
+    case 'buscar':
+        buscarClientePorId();
+        break;
+    case 'cadastrar':
+        cadastrarCliente();
+        break;
+    case 'atualizar':
+        atualizarCliente();
+        break;
+    case 'excluir':
+        excluirCliente();
         break;
     default:
         http_response_code(400); // Requisição inválida
         echo json_encode(['error' => 'Ação inválida']);
+
 }
 
-function listarContas() {
-    $contas = ContaRepository::getAllContas();
-    echo json_encode($contas);
+function listarClientes() {
+    $action = ContaRepository::getAllContas();
+    echo json_encode($action);
+}
+
+function buscarClientePorId() {
+    if($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $id = $_GET['id'];
+        $cliente = ContaRepository::getContaById($id);
+
+        if($cliente) {
+            echo json_encode($cliente);
+        } else {
+            http_response_code(404); // Cliente não encontrado
+            echo json_encode(['error' => 'Cliente não encontrado']);
+        }
+    } else {
+        http_response_code(405); // Método não permitido
+    }
+}
+
+function insertConta() {
+    if($_SERVER ['REQUEST_METHOD' === 'POST']){
+       $data = json_decode(file_get_contents("php://input"));
+       $nome = $data->nome;
+       $cpf = $data->cpf;
+
+       $success = ContaRepository::insertConta($nome, $cpf);
+       echo json_encode(['success' => $success]);
+    } else {
+        http_response_code(405);
+    }
+}
+
+function atualizarCliente() {
+    if($_SERVER ['REQUEST_METHOD' === 'POST']){
+        $data = json_decode(file_get_contents("php://input"));
+        $id = $data->id;
+        $nome = $data->nome;
+        $cpf = $data->cpf;
+ 
+        $success = ContaRepository::updateCliente($id, $nome, $cpf);
+        echo json_encode(['success' => $success]);
+    } else {
+         http_response_code(405);
+    }
+}
+
+function excluirCliente() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $data = json_decode(file_get_contents("php://input"));
+        $id = $data->id;
+
+        $success = ContaRepository::deleteCliente($id);
+        echo json_encode(['success' => $success]);
+    } else {
+        http_response_code(405);
+    }
 }
 
 ?>
--- 17/database/ContaRepository.php
+++ 17/database/ContaRepository.php
@@ -17,6 +17,42 @@ class ContaRepository {
         $connection->close();
         return $contas;
     }
+
+    public static function getContaById($id){
+        $connection = DatabaseRepository::connect();
+        $sql = "SELECT * FROM conta WHERE id = $id";
+        $result = $connection->query($sql);
+
+        $conta = null;
+        if($result->num_rows > 0){
+            $conta = $result->fetch_assoc();
+        }
+    $connection->close();
+    return $conta;
+    }
+
+    public static function insertConta($nome, $cpf){
+        $connection = DatabaseRepository::connect();
+        $sql = "INSERT INTO conta (nome,cpf) VALUES ('$nome','$cpf'";
+        $success = $connection->query($sql);
+        $connection->close();
+        return $success;
+    }
+
+    public static function updateConta($id, $nome, $cpf){
+        $connection = DatabaseRepository::connect();
+        $sql = "UPDATE conta SET nome = '$nome', cpf = '$cpf' WHERE id = $id";
+        $success = $connection->query($sql);
+        $connection->close();
+        return $success;
+    }
+
+    public static function deleteConta($id){
+        $connection = DatabaseRepository::connect();
+        $success = $connection->query("DELETE FROM conta WHERE id= $id");
+        $connection->close();
+        return $success;
+    }
 }
 
 ?>
