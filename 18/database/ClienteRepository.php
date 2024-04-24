dc8b7c04d36e097170df660f16159fcf79f181c0
BrunoArrudas <you@example.com>
2024-04-23 21:03:23 -0300

    Atualização 18.0

---
+++ 18/database/ClienteRepository.php
@@ -0,0 +1,60 @@
+<?php
+require_once 'DatabaseRepository.php';
+require_once '../model/cliente.php';
+
+class ClienteRepository {
+    
+    public static function getAllClientes() {
+        $connection = DatabaseRepository::connect();
+        $result = $connection->query("SELECT * FROM cliente");
+        
+        $clientes = [];
+        if($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $clientes[] = $row;
+            }
+        }
+
+        $connection->close();
+        return $clientes;
+    }
+
+    public static function getClienteById($id) {
+        $connection = DatabaseRepository::connect();
+        $sql = "SELECT * FROM cliente WHERE id = $id";
+        $result = $connection->query($sql);
+
+        $cliente = null;
+        if($result->num_rows > 0) {
+            $cliente = $result->fetch_assoc();
+        }
+        $connection->close();
+        return $cliente;
+    }
+
+    public static function insertCliente(Cliente $cliente) {
+        $connection = DatabaseRepository::connect();
+        $nome = $cliente->getNome();
+        $cpf = $cliente->getCpf();
+        $sql = "INSERT INTO cliente (nome, cpf) VALUES ('$nome', '$cpf')";
+        $success = $connection->query($sql);
+        $connection->close();
+        return $success;
+    }
+
+    public static function updateCliente($id, $nome, $cpf) {
+        $connection = DatabaseRepository::connect();
+        $sql = "UPDATE cliente SET nome='$nome', cpf='$cpf' WHERE id=$id";
+        $success = $connection->query($sql);
+        $connection->close();
+        return $success;
+    }
+
+    public static function deleteCliente($id) {
+        $connection = DatabaseRepository::connect();
+        $success = $connection->query("DELETE FROM cliente WHERE id=$id");
+        $connection->close();
+        return $success;
+    }
+}
+?>
