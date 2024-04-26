656d278b5fc668ac938bd1a98b589d3cf3d9b47b
BrunoArrudas <you@example.com>
2024-04-25 22:10:21 -0300

    Atualização 19

--- 19/index.php
+++ 19/index.php
@@ -19,7 +19,7 @@ switch($entity){
         require_once 'controller/ProdutoController.php';
         ProdutoController::handleRequest($action);
         break;
-    default:
+
         http_response_code(400);
         echo json_encode(['error'=>'Entidade invalida!']);
         break;
